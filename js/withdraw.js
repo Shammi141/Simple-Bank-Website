cb1be73e8dd9dc34635be991c9709635435a1edd
Shammi141 <jannatulshammi181@gmail.com>
2022-08-17 00:37:53 +0600

    withdraw validation done

--- js/withdraw.js
+++ js/withdraw.js
@@ -8,17 +8,19 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
     //console.log(newWithdrawAmount);
 
+//step8: clear the input field
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)){
+        alert('Please provide valid number');
+        return;
+    }
+
 //step3: get previous withdraw total
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-    console.log(previousWithdrawTotal);
-
-//step4: calculate total withdraw amount
-    const currentWithdrawTotal = previousWithdrawTotal +newWithdrawAmount;
-
-//step5: set total withdraw amount 
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+    //console.log(previousWithdrawTotal);
 
 //step6: get the previous balance total
     const balanceTotalElement = document.getElementById('balance-total');
@@ -26,11 +28,23 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
     //console.log(previousBalanceTotal);
 
-//step7: calculate new balance total and set the balance total
+//validation for withdrawing more than your balance
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('You can not withdraw more than your balance amount');
+        return;
+    }
+
+
+    //--------------step4-5: code age theke exicute kore felbe & withdraw money update kore felbe tai condition er niche dite hobe
+    //step4: calculate total withdraw amount
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    //step5: set total withdraw amount 
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    //step7: calculate new balance total and set the balance total
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-    balanceTotalElement.innerText = newBalanceTotal; 
-
-//step8: clear the input field
-    withdrawField.value = '';
-
+    balanceTotalElement.innerText = newBalanceTotal;
 });
+
+
